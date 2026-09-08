0bfcd86ace53af0500dfe1ff0d32e88f9727c889
Rafael Quisbert <rafael.quisbert@ucb.edu.bo>
2026-09-07 20:42:22 -0400

    Implementacion de excepciones

--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -1,5 +1,7 @@
 namespace BibliotecaApp.Models
+{
 public record Prestamo(string, CodigoLibro, string IdUsuario, DateTime FechaPrestamo, DateTime? FechaDevolucion)
 {
     public bool Activo => FechaDevolucion == null;
+}
 }
